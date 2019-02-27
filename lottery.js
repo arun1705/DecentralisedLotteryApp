7eee8de621fda8ab65f71cbf92285391949aae34
arun1705 <arun.hosamani@nelito.com>
2019-02-27 11:56:11 +0530

    getting web3 installed

--- lottery.js
+++ lottery.js
@@ -1,4 +1,3 @@
-
 import web3 from './web3.js';
 const address = '0xB0BC553Ce8815f62DbC24539d5d738ffD9f1011d';
 const abi = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"participants","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"enterlottery","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
