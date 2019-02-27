578d3de5f47f1b31b832dc0f7d6580ce28d79f93
arun1705 <arun.hosamani@nelito.com>
2019-02-27 11:21:37 +0530

    deployed to rinkeby network

--- deploy.js
+++ deploy.js
@@ -1,5 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
+const {interface,bytecode} = require('./compile.js');
 const mnemonic = 'tomato rate sheriff course end labor topic above curious this rail group'
 
 const web3 = new Web3(new HDWalletProvider(
@@ -11,5 +12,18 @@ const web3 = new Web3(new HDWalletProvider(
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
     console.log(accounts);
+    console.log('Contract is deployed by the manager with address ', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: '0x' + bytecode
+        })
+        .send({
+            gas: '2000000',
+            from: accounts[0]
+        })
+
+    console.log('Contract deployed to address', result.options.address);
+    //0xB0BC553Ce8815f62DbC24539d5d738ffD9f1011d
 }
 deploy();
